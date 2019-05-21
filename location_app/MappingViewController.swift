7ca667a6a1a8091c519ac2061f0af05107ddc99d
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-05-22 02:00:56 +0900

    [start] mapping

---
+++ location_app/MappingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MappingViewController.swift
+//  location_app
+//
+//  Created by 磯崎裕太 on 2019/05/22.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MappingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

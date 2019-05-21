59b24627cbb42cb80b5fa202a5b6c66672ee78a7
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-05-22 01:29:09 +0900

    Initialize Commit

---
+++ location_app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  location_app
+//
+//  Created by 磯崎裕太 on 2019/05/22.
+//  Copyright © 2019 HoleFillingCo.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var idoLabel: UILabel!
+    @IBOutlet weak var keidoLabel: UILabel!
+    @IBOutlet weak var hyoukouLabel: UILabel!
+    @IBOutlet weak var henkakuLabel: UILabel!
+    @IBOutlet weak var houiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
